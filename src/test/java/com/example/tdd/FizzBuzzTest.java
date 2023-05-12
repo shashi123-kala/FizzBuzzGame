6afbbf1516fcb5b2db881ab93b456f26cedd63f3
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-12 08:02:00 +0530

    Add: Initialize fizz Buzz game

---
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package com.example.tdd;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+	
+	@Test
+    public void initializeGame() {
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        assertNotNull( fizzBuzz );
+    }
+
+}
