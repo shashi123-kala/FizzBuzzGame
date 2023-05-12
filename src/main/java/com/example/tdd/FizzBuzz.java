a7c6e36528a108d95773118f51b30a064e7c316a
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-12 08:48:20 +0530

    Add:start the game with number one

--- src/main/java/com/example/tdd/FizzBuzz.java
+++ src/main/java/com/example/tdd/FizzBuzz.java
@@ -2,4 +2,8 @@ package com.example.tdd;
 
 public class FizzBuzz {
 
+public String play(int number) {		
+		return String.valueOf(number);		
+		
+	}
 }
--- src/test/java/com/example/tdd/FizzBuzzTest.java
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -2,6 +2,7 @@ package com.example.tdd;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzTest {
@@ -10,6 +11,14 @@ public class FizzBuzzTest {
     public void initializeGame() {
         FizzBuzz fizzBuzz=new FizzBuzz();
         assertNotNull( fizzBuzz );
+    }
+	
+	@Test
+    public void numberEqualToOne() {
+		FizzBuzz fizzBuzz=new FizzBuzz();
+		int number = 1;
+		String numberReturned = fizzBuzz.play(number);    
+		Assert.assertEquals("1",numberReturned );
     }
 
 }
