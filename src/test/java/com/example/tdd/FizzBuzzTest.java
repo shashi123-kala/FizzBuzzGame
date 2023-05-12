691dda8920a47a54038ab74e1bfeb99143f0c773
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-12 09:38:35 +0530

    Refractor: Extracting a object  and defining globally

--- src/test/java/com/example/tdd/FizzBuzzTest.java
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -3,21 +3,23 @@ package com.example.tdd;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 	
-	@Test
+	private  FizzBuzz fizzBuz;
+	
+	@Before
     public void initializeGame() {
-        FizzBuzz fizzBuzz=new FizzBuzz();
-        assertNotNull( fizzBuzz );
+		fizzBuz = new FizzBuzz();
+		assertNotNull(fizzBuz);         
     }
 	
 	@Test
     public void numberEqualToOne() {
-		FizzBuzz fizzBuzz=new FizzBuzz();
 		int number = 1;
-		String numberReturned = fizzBuzz.play(number);    
+		String numberReturned = fizzBuz.play(number);    
 		Assert.assertEquals("1",numberReturned );
     }
 
