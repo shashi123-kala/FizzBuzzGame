dc19788317d28707ab30dd56bff3b4858ccbc7da
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-12 10:43:55 +0530

    Add: Play with number nine

--- src/test/java/com/example/tdd/FizzBuzzTest.java
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -64,11 +64,20 @@ public class FizzBuzzTest {
 		String numberReturned = fizzBuz.play(number);    
 		Assert.assertEquals("7",numberReturned );
     }
+	
 	@Test
     public void numberEqualToEight() {
 		int number = 8;
 		String numberReturned = fizzBuz.play(number);    
 		Assert.assertEquals("8",numberReturned );
     }
+	
+	@Test
+    public void numberEqualToNine() {
+		int number = 9;
+		String numberReturned = fizzBuz.play(number);    
+		Assert.assertEquals("Fizz",numberReturned );
+    }
 }
+
 
