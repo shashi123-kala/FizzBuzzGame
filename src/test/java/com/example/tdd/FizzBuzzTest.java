ba1fa62ba9eac2bdea33fd529cdc5b69ce82bd72
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-15 10:59:52 +0530

    Refractor: used Parameterized test annotaion to avoid multiple methods

--- src/test/java/com/example/tdd/FizzBuzzTest.java
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 	
@@ -20,111 +23,30 @@ public class FizzBuzzTest {
 		assertNotNull(fizzBuz);         
     }
 	
-	@Test
-    public void numberEqualToOne() {
-		int number = 1;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("1",numberReturned );
-    }
 	
-	@Test
-    public void numberEqualToTwo() {
-		int number = 2;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("2",numberReturned );
-    }
+	@ParameterizedTest
+	@CsvSource({
+		"1, 1",
+		"2, 2",
+		"3, Fizz",
+		"4, 4",
+		"5, Buzz",
+		"6, Fizz",
+		"7, 7",
+		"8, 8",
+		"9, Fizz",
+		"10, Buzz",
+		"11, 11",
+		"12, Fizz",
+		"13, 13",
+		"14, 14",
+		"15, FizzBuzz"
+	})
+	 public void numberEqualToTwo1(int number, String expected) {
+	    assertEquals(expected , fizzBuz.play(number));
 
-	@Test
-    public void numberEqualToThree() {
-		int number = 3;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_THREE,numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToFour() {
-		int number = 4;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("4",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToFive() {
-		int number = 5;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_FIVE,numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToSix() {
-		int number = 6;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_THREE,numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToSeven() {
-		int number = 7;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("7",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToEight() {
-		int number = 8;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("8",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToNine() {
-		int number = 9;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_THREE,numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToTen() {
-		int number = 10;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_FIVE,numberReturned );
-    }
-	
+	}
 
-	@Test
-    public void numberEqualToEleven() {
-		int number = 11;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("11",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToTweleve() {
-		int number = 12;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("Fizz",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToThirteen() {
-		int number = 13;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("13",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToFourteen() {
-		int number = 14;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals("14",numberReturned );
-    }
-	
-	@Test
-    public void numberEqualToFifteen() {
-		int number = 15;
-		String numberReturned = fizzBuz.play(number);    
-		Assert.assertEquals(NUMBER_DIVISBLEBY_BOTH_THREE_AND_FIVE,numberReturned );
-    }
 }
 
 
